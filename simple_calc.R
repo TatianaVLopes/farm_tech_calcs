d960175f086e52d7012349ab966daaf72d9fd984
Ana Kolodji <meuemail@anakolodji.com>
2024-09-17 21:32:33 -0300

    Minimo, máximo e variancia

--- simple_calc.R
+++ simple_calc.R
@@ -4,3 +4,12 @@ areas <- c(1000, 1500, 2000, 3000)
 # Calcular a média das áreas:
 media_areas <- mean(areas)
 print(paste("Média das áreas:", round(media_areas, 2)))
+
+# Obter o mínimo e o máximo das áreas
+min_area <- min(areas)
+max_area <- max(areas)
+print(paste("Mínimo:", min_area, "Máximo:", max_area))
+
+# Calcular a variância das áreas
+variancia_areas <- var(areas)
+print(paste("Variância das áreas:", round(variancia_areas, 2)))
